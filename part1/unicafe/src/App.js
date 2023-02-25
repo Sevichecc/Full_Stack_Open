6db0baa17f6b577dbed1ef55ec6cea35b85ad45c
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-02-26 00:05:09 +0800

    Amend unicafe,ref #2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,13 +13,33 @@ const StatisticLine = ({ text, value }) => {
   );
 };
 
+const Statistic = ({ good, neutral, bad, all, average, postive }) => {
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text='Good' value={good} />
+          <StatisticLine text='Netural' value={neutral} />
+          <StatisticLine text='Bad' value={bad} />
+          <StatisticLine text='All' value={all} />
+          <StatisticLine text='Average' value={average} />
+          <StatisticLine text='Postive' value={postive} />
+        </tbody>
+      </table>
+    );
+  }
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const average = (good + bad * -1) / 2;
-  const postive = good / (good + neutral + bad) || 0;
+  let all = good + neutral + bad;
+  let average = (good + bad * -1) / all;
+  let postive = (good / (good + neutral + bad)) * 100 + ' %';
 
   const handleGood = () => {
     setGood(good + 1);
@@ -41,17 +61,15 @@ const App = () => {
         <Button title='Netural' onClick={handleNeutral} />
         <Button title='Bad' onClick={handleBad} />
       </div>
-
       <h2>Statistic</h2>
-      <table>
-        <tbody>
-          <StatisticLine text='Good' value={good} />
-          <StatisticLine text='Netural' value={neutral} />
-          <StatisticLine text='Bad' value={bad} />
-          <StatisticLine text='Average' value={average} />
-          <StatisticLine text='Postive' value={postive} />
-        </tbody>
-      </table>
+      <Statistic
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        postive={postive}
+      />
     </div>
   );
 };
